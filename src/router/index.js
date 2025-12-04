a510bcad4f7eb8fa13692a02f935e79529b556cd
Randy Setiawan <chenlian1207@gmail.com>
2025-12-04 15:39:04 +0700

    remove legacy landing & presentation views and replace with new Main layout

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import MainView from "../views/Presentation/MainView.vue";
+import MainView from "../views/Main/MainView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
